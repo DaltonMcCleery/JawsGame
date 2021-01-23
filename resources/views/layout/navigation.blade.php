7359a3091bbdba4c5e909029e602ff5a35ef5078
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-23 15:18:42 -0600

    Fix: navigation wording

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar is-dark">
     <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
+        <a class="navbar-item" href="/">
             @include('svgs.logo')
         </a>
-        <div class="navbar-burger" data-target="navbarExampleTransparentExample">
+        <div class="navbar-burger" data-target="navigation-menu">
             <span></span>
             <span></span>
             <span></span>
         </div>
     </div>
 
-    <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div id="navigation-menu" class="navbar-menu">
         <div class="navbar-start"></div>
 
         <div class="navbar-end">
