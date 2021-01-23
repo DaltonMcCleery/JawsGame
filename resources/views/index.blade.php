5a1445b1a696c234188d26cb45f9c8c26765d9b3
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-23 16:22:32 -0600

    Updating homepage hero

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,14 @@
 @extends('wrapper')
 @section('content')
 
-    <section class="hero is-dark is-large">
+    <section id="index-hero" class="hero is-dark is-large" style="background: url('{{ asset('images/background.jpg') }}')">
+{{--        <iframe id="homepage-video" src="https://www.youtube.com/embed/2I91DJZKRxs?autoplay=1&modestbranding=1&rel=0"--}}
+{{--                allowTransparency="true" frameborder="0" allow="autoplay; muted" volume="0" allowfullscreen></iframe>--}}
         <div class="hero-body">
             <div class="container">
                 <h1 class="title">
-                    Jaws
-                </h1>
-                <h2 class="subtitle">
                     You're gonna need a bigger boat...
-                </h2>
+                </h1>
             </div>
         </div>
     </section>
