a917ee842ef5d7f73fabb5b721e9760795126e1f
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-23 09:14:13 -0600

    Adds: navigation links

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -17,7 +17,21 @@
             <div class="navbar-item">
                 <div class="field is-grouped">
                     <p class="control">
-
+                        @auth
+                            <a href="/profile" class="button is-danger" style="background-color: #e40403;">
+                                <strong>Profile</strong>
+                            </a>
+                            <a href="/logout" class="button is-light">
+                                Logout
+                            </a>
+                        @else
+                            <a href="/register" class="button is-danger" style="background-color: #e40403;">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a href="/login" class="button is-light">
+                                Log in
+                            </a>
+                        @endauth
                     </p>
                 </div>
             </div>
