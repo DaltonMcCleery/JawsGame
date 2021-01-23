9e007b623879d6d645298d16a15d4d6fda2ec787
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-23 08:54:42 -0600

    Replace welcome view with wrapper

---
+++ resources/views/wrapper.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Jaws</title>
+
+        <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}" type="text/css" media="all" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+              integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+              crossorigin="anonymous">
+
+        @livewireStyles
+    </head>
+    <body class="antialiased">
+
+        @include('layout.navigation')
+        @include('layout.flash_messages')
+
+        @yield('content')
+
+        @include('layout.footer')
+
+        <script src="{{ mix('dist/js/app.js') }}"></script>
+        @livewireScripts
+        @yield('scripts')
+    </body>
+</html>
