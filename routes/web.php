686113d7fe1ee6151ddb8b8c42c83fd1478c42e0
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-23 14:44:27 -0600

    Adds: default Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LobbyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/home', '/', 301);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/how-to-play', [HomeController::class, 'howToPlay'])->name('how_to_play');
+
+// --- Authenticated Customer --- //
+Route::middleware('players')->group(function () {
+
+    // ---> GAME
+    Route::prefix('play')->group(function () {
+        Route::get('/', [LobbyController::class, 'index'])->name('find.game');
+        Route::post('/join', [LobbyController::class, 'joinPrivateGame'])->name('join.game');
+        Route::get('/lobby/{session_id}', [LobbyController::class, 'lobby'])->name('game.lobby');
+        Route::get('/game/{session_id}', [LobbyController::class, 'play'])->name('play.game');
+    });
+
+    Route::post('create/game', [LobbyController::class, 'createGame'])->name('create.game');
+
+    // ---> PROFILE
+    Route::prefix('profile')->group(function () {
+        Route::get('/', 'ProfileController@index')->name('user.profile');
+        Route::get('/update', 'ProfileController@updateProfile')->name('user.profile.update');
+    });
 });
+
+Auth::routes();
+Route::post('/login', 'Auth\LoginController@login');
+Route::get('/logout', 'Auth\LoginController@logout');
