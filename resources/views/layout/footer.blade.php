733c9df260eeab90376dba71a54beee9e74b594a
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-23 10:38:16 -0600

    Updates: footer callouts

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="footer">
     <div class="content has-text-centered">
         <p>
-            <strong>Jaws Game</strong> by <a href="https://github.com/DaltonMcCleery">Dalton McCleery</a>.
+            <strong>JAWS Board Game</strong> by <a href="https://www.ravensburger.org">Ravensburger</a>.
+        </p>
+        <p>
+            Adapted for web by <a href="https://github.com/DaltonMcCleery">Dalton McCleery</a>
         </p>
     </div>
 </footer>
