fa0fa220384acfb273137ccce68ff6f32e6de364
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-24 08:35:24 -0600

    Adds: error blades

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('wrapper')
+@section('content')
+    <section class="hero is-medium is-dark is-bold">
+        <div class="container" style="text-align: center">
+            <div class="hero-body">
+                <h1 class="title">404</h1>
+                <h2 class="subtitle">
+                    Must have gone under the boat
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,13 @@
+@extends('wrapper')
+@section('content')
+    <section class="hero is-medium is-dark is-bold">
+        <div class="container" style="text-align: center">
+            <div class="hero-body">
+                <h1 class="title">500</h1>
+                <h2 class="subtitle">
+                    The holiday ham was eaten
+                </h2>
+            </div>
+        </div>
+    </section>
+@endsection
