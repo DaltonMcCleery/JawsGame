fad09766d98c363714262c2ef16d8e310dfe70da
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-24 20:24:50 -0600

    Build out adjacency rules

--- app/Traits/ActOneRules.php
+++ app/Traits/ActOneRules.php
@@ -42,45 +42,157 @@ trait ActOneRules {
         'Launch a Barrel'
     ];
 
+    private $adjacentSpaces = [
+        'Space_1' => [
+            'Space_2',
+            'Space_3',
+            'North_Beach',
+            'Space_5'
+        ],
+        'Space_2' => [
+            'Space_1',
+            'Space_6',
+            'East_Beach',
+            'Space_4'
+        ],
+        'Space_3' => [
+            'Space_1',
+            'Space_4',
+            'West_Beach',
+            'Space_7'
+        ],
+        'Space_4' => [
+            'Space_2',
+            'Space_3',
+            'South_Beach',
+            'Space_8'
+        ],
+        'Space_5' => [
+            'Space_1',
+            'North_Beach',
+            'West_Beach',
+            'Space_7',
+            'Shop'
+        ],
+        'Space_6' => [
+            'Space_2',
+            'North_Beach',
+            'East_Beach',
+            'South_Beach',
+            'Space_8',
+            'Shop'
+        ],
+        'Space_7' => [
+            'Space_3',
+            'South_Beach',
+            'West_Beach',
+            'Space_5',
+            'Shop'
+        ],
+        'Space_8' => [
+            'Space_4',
+            'East_Beach',
+            'South_Beach',
+            'Space_6'
+        ],
+        'North_Beach' => [
+            'Space_1',
+            'Space_5',
+            'Space_6',
+            'Shop'
+        ],
+        'East_Beach' => [
+            'Space_2',
+            'Space_6',
+            'Space_8'
+        ],
+        'South_Beach' => [
+            'Space_4',
+            'Space_6',
+            'Space_7',
+            'Space_8',
+            'Shop'
+        ],
+        'West_Beach' => [
+            'Space_3',
+            'Space_5',
+            'Space_7'
+        ],
+    ];
+
     /**
      * Determine if a move was valid based on Character rules + Game's State at time of Action
      *
      * @param $character
      * @param $action
+     * @param $space
+     * @param $currentActionState
      * @param $gameState
      * @return boolean
      */
-    public function isValidAction($character, $action, $gameState) {
+    public function isValidAction($character, $action, $space, $currentActionState, $gameState) {
         switch ($character) {
             case 'shark':
                 if (in_array($action, $this->sharkActions)) {
-                    return true;
+                    if ($this->isNotOutOfMoves($character, $currentActionState, $gameState)) {
+                        if ($this->isMoveAdjacent($character, $action, $space, $currentActionState, $gameState)) {
+                            return true;
+                        }
+                    }
                 }
 
                 return false;
 
             case 'brody':
                 if (in_array($action, $this->brodyActions)) {
-                    return true;
+                    if ($this->isNotOutOfMoves($character, $currentActionState, $gameState)) {
+                        if ($this->isMoveAdjacent($character, $action, $space, $currentActionState, $gameState)) {
+                            return true;
+                        }
+                    }
                 }
 
                 return false;
 
             case 'hooper':
                 if (in_array($action, $this->hooperActions)) {
-                    return true;
+                    if ($this->isNotOutOfMoves($character, $currentActionState, $gameState)) {
+                        if ($this->isMoveAdjacent($character, $action, $space, $currentActionState, $gameState)) {
+                            return true;
+                        }
+                    }
                 }
 
                 return false;
 
             case 'quint':
                 if (in_array($action, $this->quintActions)) {
-                    return true;
+                    if ($this->isNotOutOfMoves($character, $currentActionState, $gameState)) {
+                        if ($this->isMoveAdjacent($character, $action, $space, $currentActionState, $gameState)) {
+                            return true;
+                        }
+                    }
                 }
 
                 return false;
         }
 
         return false;
+    }
+
+    private function isNotOutOfMoves($character, $currentActionState, $gameState): bool {
+        return (count($currentActionState) <= $gameState[$character.'_moves']);
+    }
+
+    private function isMoveAdjacent($character, $action, $space, $currentActionState, $gameState): bool {
+        if (str_contains($action, 'Move')) {
+            if (in_array($space, $this->adjacentSpaces[$gameState[$character.'_position']])) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return true;
     }
 }
