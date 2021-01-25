60915f579e372c9503e08b634b7fab5ef5f0409d
DaltonMcCleery <daltonmccleery@gmail.com>
2021-01-25 15:24:57 -0600

    Fix: running action state for multiples of the same action

--- app/Traits/ActOneActions.php
+++ app/Traits/ActOneActions.php
@@ -25,35 +25,52 @@ trait ActOneActions {
                 // Check for Michael
                 if (isset($gameState['michael_position']) && $gameState['michael_position'] === ($location.'_Swimmers')) {
                     $state_changes[$location.'_Swimmers'] = $gameState[$location.'_Swimmers'] - 2;
+                    // Reverse changes for continued actions
+                    $gameState[$location.'_Swimmers'] = $state_changes[$location.'_Swimmers'];
                 } else {
                     // Remove Swimmer from Beach
                     $state_changes[$location.'_Swimmers'] = $gameState[$location.'_Swimmers'] - 1;
+                    // Reverse changes for continued actions
+                    $gameState[$location.'_Swimmers'] = $state_changes[$location.'_Swimmers'];
                 }
             }
 
             if (str_contains($action, 'Eat all Swimmers')) {
                 // Remove ALL Swimmers from Beach
                 $state_changes[$location.'_Swimmers'] = 0;
+                // Reverse changes for continued actions
+                $gameState[$location.'_Swimmers'] = 0;
             }
 
             // Barrels
             if (str_contains($action, 'Pick up 1 Barrel')) {
                 $state_changes['brody_barrels'] = $gameState['brody_barrels'] + 1;
+                // Reverse changes for continued actions
+                $gameState['brody_barrels'] = $state_changes['brody_barrels'];
             }
 
             if (str_contains($action, 'Drop 1 Barrel')) {
                 $state_changes['brody_barrels'] = $gameState['brody_barrels'] - 1;
                 $state_changes[$location.'_barrels'] = $gameState[$location.'_barrels'] + 1;
+                // Reverse changes for continued actions
+                $gameState['brody_barrels'] = $state_changes['brody_barrels'];
+                $gameState[$location.'_barrels'] = $state_changes[$location.'_barrels'];
             }
 
             if (str_contains($action, 'Pick up any or all Barrels')) {
                 $state_changes[$character.'_barrels'] = $gameState[$character.'_barrels'] + $gameState[$location.'_barrels'];
                 $state_changes[$location.'_barrels'] = 0;
+                // Reverse changes for continued actions
+                $gameState[$character.'_barrels'] = $state_changes[$character.'_barrels'];
+                $gameState[$location.'_barrels'] = 0;
             }
 
             if (str_contains($action, 'Give all Barrels to Quint')) {
                 $state_changes['quint_barrels'] = $gameState['quint_barrels'] + $gameState[$character.'_barrels'];
                 $state_changes[$character.'_barrels'] = 0;
+                // Reverse changes for continued actions
+                $gameState['quint_barrels'] = $state_changes['quint_barrels'];
+                $gameState[$character.'_barrels'] = $state_changes[$character.'_barrels'];
             }
 
             // Shark Abilities
