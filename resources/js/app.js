ef08b52a7529f94269e641ec82f1a40c447f6cc1
DaltonMcCleery <daltonmccleery@gmail.com>
2022-01-29 12:10:23 -0600

    Adds: Alpine JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,8 @@
-window._ = require('lodash');
+import Alpine from 'alpinejs'
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
+window.Alpine = Alpine
 
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+Alpine.start()
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -27,27 +21,4 @@ window.Echo = new Echo({
     wsPort: 6001,
     wssPort: 6001,
     forceTLS: true
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if (navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
-                // Get the target from the "data-target" attribute
-                const target = document.getElementById(el.dataset.target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                target.classList.toggle('is-active');
-            });
-        });
-    }
-
 });
