479162b2dba0a0c5a47555cf4f9073b98b735220
DaltonMcCleery <daltonmccleery@gmail.com>
2022-04-29 21:41:38 -0500

    Fix: available games

--- resources/views/livewire/available-games.blade.php
+++ resources/views/livewire/available-games.blade.php
@@ -7,13 +7,13 @@
                     <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 48 48" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M34 40h10v-4a6 6 0 00-10.712-3.714M34 40H14m20 0v-4a9.971 9.971 0 00-.712-3.714M14 40H4v-4a6 6 0 0110.713-3.714M14 40v-4c0-1.313.253-2.566.713-3.714m0 0A10.003 10.003 0 0124 26c4.21 0 7.813 2.602 9.288 6.286M30 14a6 6 0 11-12 0 6 6 0 0112 0zm12 6a4 4 0 11-8 0 4 4 0 018 0zm-28 0a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
-                    @if(Auth::user()->role === 'ADMIN')
+                    @if(auth()->user()->role === 'ADMIN')
                         <h2 class="mt-2 text-lg font-medium text-gray-900">Create a New Game</h2>
                     @else
                         <h2 class="mt-2 text-lg font-medium text-gray-900">Join a Game</h2>
                     @endif
                 </div>
-                @if(Auth::user()->role === 'ADMIN')
+                @if(auth()->user()->role === 'ADMIN')
                     <form action="#" class="mt-6 flex">
                         <input type="text" placeholder="Game Password (optional)" wire:model="game_id" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
                         <button type="submit" class="ml-4 flex-shrink-0 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" wire:click="createGame">
@@ -34,14 +34,13 @@
                         <li class="py-4 flex items-center justify-between space-x-3">
                             <div class="min-w-0 flex-1 flex items-center space-x-3">
                                 <div class="min-w-0 flex-1">
-                                    <p class="text-sm font-medium text-gray-900 truncate">{{ $game->Host->name }}'s Game</p>
+                                    <p class="text-sm font-medium text-gray-900 truncate">{{ $game->host->name }}'s Game</p>
                                     <p class="text-sm font-medium text-gray-500 truncate">Created on {{ $game->created_at->format('F j, Y') }}</p>
                                 </div>
                             </div>
                             <div class="flex-shrink-0">
                                 <button type="button" class="inline-flex items-center py-2 px-3 border border-transparent rounded-full bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                         wire:click="joinGame('{{ $game->id }}')">
-                                    <!-- Heroicon name: solid/plus-sm -->
                                     <svg class="-ml-1 mr-0.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
                                     </svg>
@@ -59,6 +58,7 @@
                             </div>
                         </li>
                     @endforelse
+                </ul>
             </div>
         </div>
     @else
