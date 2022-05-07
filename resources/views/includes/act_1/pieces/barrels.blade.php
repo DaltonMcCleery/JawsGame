440dec15504dc0699f93de66783c00149ffdc802
DaltonMcCleery <daltonmccleery@gmail.com>
2022-05-07 10:58:42 -0500

    Updates: position scaling for Barrels and Swimmers

--- resources/views/includes/act_1/pieces/barrels.blade.php
+++ resources/views/includes/act_1/pieces/barrels.blade.php
@@ -1,7 +1,12 @@
 @if(isset($gameState['Shop_barrels']) && $gameState['Shop_barrels'] > 0)
-    <span id="shop-barrels" title="{{ $gameState['Shop_barrels'] }}" class="barrels-count">
-        <img src="{{ asset('images/barrel.png') }}" alt="Barrel"/>
-    </span>
+    <div id="shop-barrels" title="{{ $gameState['Shop_barrels'] }}" class="absolute bottom-[46%] left-[45%] w-[55px]">
+        <div class="relative">
+            <img src="{{ asset('images/barrel.png') }}" alt="Barrel"/>
+            <span class="absolute px-2 inline-flex md:text-lg text-2xl font-bold rounded-full top-[-15px] right-[-15px] bg-gray-700 text-white">
+                {{ $gameState['Shop_barrels'] }}
+            </span>
+        </div>
+    </div>
 @endif
 
 @if(isset($gameState['Space_8_barrels']) && $gameState['Space_8_barrels'] > 0)
