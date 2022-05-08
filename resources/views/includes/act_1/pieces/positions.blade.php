1aa59efe7333203c8834cfb945eb4c8f5731ffc4
DaltonMcCleery <daltonmccleery@gmail.com>
2022-05-08 11:17:50 -0500

    Updates: character positions

--- resources/views/includes/act_1/pieces/positions.blade.php
+++ resources/views/includes/act_1/pieces/positions.blade.php
@@ -1,15 +1,37 @@
-@if(isset($gameState['shark_position']) && (($gameState['active_character'] === 'shark') || $gameState['show_shark']))
-    <div id="shark-position" class="absolute {{ $gameState['shark_position'] }}"></div>
+@if(isset($gameState['shark_position']))
+    @if((auth()->id() === $game->player && $gameState['active_character'] === 'shark') || (auth()->id() === $game->monitor && $gameState['show_shark'])))
+        <div id="shark-position" class="absolute w-[50px] {{ $gameState['shark_position'] }}">
+            @if (($gameState['active_character'] === 'shark') || $gameState['show_shark'])
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-custom-red opacity-50"></span>
+            @endif
+            <img src="{{ asset('storage/shark.png') }}" alt="Shark" class="z-10"/>
+        </div>
+    @endif
 @endif
 
 @if(isset($gameState['brody_position']))
-    <div id="brody-position" class="{{ $gameState['brody_position'] }}"></div>
+    <div id="brody-position" class="absolute w-[50px] {{ $gameState['brody_position'] }}">
+        @if (($gameState['active_character'] === 'brody'))
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-brody opacity-50"></span>
+        @endif
+        <img src="{{ asset('storage/brody.png') }}" alt="Brody" class="z-10"/>
+    </div>
 @endif
 
 @if(isset($gameState['hooper_position']))
-    <div id="hooper-position" class="{{ $gameState['hooper_position'] }}"></div>
+    <div id="hooper-position" class="absolute w-[50px] {{ $gameState['hooper_position'] }}">
+        @if (($gameState['active_character'] === 'hooper'))
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-hooper opacity-50"></span>
+        @endif
+        <img src="{{ asset('storage/hooper.png') }}" alt="Hooper" class="z-10"/>
+    </div>
 @endif
 
 @if(isset($gameState['quint_position']))
-    <div id="quint-position" class="{{ $gameState['quint_position'] }}"></div>
+    <div id="quint-position" class="absolute w-[50px] {{ $gameState['quint_position'] }}">
+        @if (($gameState['active_character'] === 'quint'))
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-quint opacity-50"></span>
+        @endif
+        <img src="{{ asset('storage/quint.png') }}" alt="Quint" class="z-10"/>
+    </div>
 @endif
