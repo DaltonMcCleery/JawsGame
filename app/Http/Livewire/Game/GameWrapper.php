2d02f17d80c3bcc26b4869f2a5e033a16e57b05b
DaltonMcCleery <daltonmccleery@gmail.com>
2022-05-21 11:16:18 -0500

    Moves: ending videos to Act I over check

--- app/Http/Livewire/Game/GameWrapper.php
+++ app/Http/Livewire/Game/GameWrapper.php
@@ -84,13 +84,13 @@ class GameWrapper extends Component
                 $this->gameState['brody_moves'] = 0;
                 $this->gameState['hooper_moves'] = 0;
                 $this->gameState['quint_moves'] = 0;
-            }
 
-            if ($this->gameState['shark_barrels'] >= 2) {
-                $this->gameState['video'] = 'barrel-2';
-            }
-            elseif ($this->gameState['swimmers_eaten'] >= 9) {
-                $this->gameState['video'] = 'shark-win';
+                if ($this->gameState['shark_barrels'] >= 2) {
+                    $this->gameState['video'] = 'barrel-2';
+                }
+                elseif ($this->gameState['swimmers_eaten'] >= 9) {
+                    $this->gameState['video'] = 'shark-win';
+                }
             }
 
             $this->emit('refreshActOneState', $this->gameState);
