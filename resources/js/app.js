4bc2ce361081646975620f249c70ae0f4d0a3e6f
DaltonMcCleery <daltonmccleery@gmail.com>
2022-05-22 14:05:14 -0500

    Updates: broadcasting for Pusher JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,9 +10,10 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'your-pusher-key',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    // wsHost: window.location.hostname,
+    // wsPort: 6001,
+    // wssPort: 6001,
     forceTLS: true
 });
