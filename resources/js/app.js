dcc00b5d4162a0ac0f093ab4dae810a70835931f
DaltonMcCleery <daltonmccleery@gmail.com>
2022-05-29 14:55:45 -0500

    Force: pusher key

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,8 +10,8 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: 'f65d212959ca6e55ed1f',
+    cluster: 'us2',
     // wsHost: window.location.hostname,
     // wsPort: 6001,
     // wssPort: 6001,
