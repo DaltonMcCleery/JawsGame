1e79ba3e81d1a74976794ebaff41b7a751d352a5
DaltonMcCleery <daltonmccleery@gmail.com>
2022-06-04 11:20:25 -0500

    Fix: video loading

--- resources/views/components/video.blade.php
+++ resources/views/components/video.blade.php
@@ -3,18 +3,18 @@
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity z-10"></div>
 
         <div class="absolute top-[10%] right-0 left-0 inline-block overflow-hidden w-full z-50">
-            <video-js id="video" class="mx-auto">
+            <video-js id="video-{{ $gameState['video'] }}" class="mx-auto">
                 <source src="{{ asset('videos/'.$gameState['video'].'.mov') }}">
             </video-js>
         </div>
 
         <script>
             setTimeout(function () {
-                let player = videojs('video', {}, function onPlayerReady() {
-                    // In this context, `this` is the player that was created by Video.js.
+                window.videoPlayer = videojs('video-{{ $gameState['video'] }}', {}, function onPlayerReady() {
+                    this.src({src: '{{ asset('videos/'.$gameState['video'].'.mov') }}'});
+
                     this.play();
 
-                    // How about an event listener?
                     this.on('ended', function() {
                         window.livewire.emit('onVideoEnd');
                     });
@@ -22,4 +22,8 @@
             }, 1000);
         </script>
     @endif
+
+    <script>
+        window.videoPlayer = null;
+    </script>
 </div>
