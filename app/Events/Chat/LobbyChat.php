c36757345667ffee77eb164d9a081c56f4c2dfdc
DaltonMcCleery <daltonmccleery@gmail.com>
2022-06-04 20:09:07 -0500

    Updates: Chat event naming

--- app/Events/Chat/LobbyChat.php
+++ app/Events/Chat/LobbyChat.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class lobbyChat implements ShouldBroadcast
+class LobbyChat implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- app/Events/Chat/SyncLobbyChat.php
+++ app/Events/Chat/SyncLobbyChat.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class syncLobbyChat implements ShouldBroadcast
+class SyncLobbyChat implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
